c2e1d8bd65e0fcbe86987db5a137c2a4a3507d0b
rseedorff <mailtorf@gmail.com>
2015-11-29 16:18:24 +0100

    Changed response to JSON object

--- helloWorld.py
+++ helloWorld.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
+# This route will return a object in JSON format
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return jsonify(result='Hello World !')
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
